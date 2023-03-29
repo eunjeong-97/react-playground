98299c0e61405c68176c114e7c8b7c3976c9dbd5
eunjeong-97 <eoss7777@naver.com>
2023-03-29 11:20:43 +0900

    230329: [RN] 리액트 네비게이션 시작! 완강
    - 리액트 내비게이션 화면전환하기
    - 라우트 params와 헤더옵션들
    - relative, absolute 모달창

--- RN/App.tsx
+++ RN/App.tsx
@@ -4,7 +4,14 @@ import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
-import {Text, TouchableHighlight, View, Pressable} from 'react-native';
+import {
+  Text,
+  TouchableHighlight,
+  View,
+  Pressable,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 import {useCallback} from 'react';
 
 type RootStackParamList = {
@@ -14,7 +21,7 @@ type RootStackParamList = {
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
 
-function HomeScreen({navigation}: HomeScreenProps) {
+function HomeScreen({navigation, route}: HomeScreenProps) {
   const onClick = useCallback(() => {
     navigation.navigate('Details');
   }, [navigation]);
@@ -56,11 +63,17 @@ function DetailsScreen({navigation}: DetailsScreenProps) {
   );
 }
 
+const {width: WINDOW_WIDTH} = Dimensions.get('window');
+
 // 페이지시작
 const Stack = createNativeStackNavigator<RootStackParamList>();
 function App() {
   return (
     <NavigationContainer>
+      {/* 여기에 absolute View를 그리면 Stack.Navigator에 덮어씌워진다 */}
+      <View style={{position: 'absolute'}}>
+        <Text>모달창</Text>
+      </View>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
@@ -76,6 +89,79 @@ function App() {
           {props => <DetailsScreen {...props} />}
         </Stack.Screen> */}
       </Stack.Navigator>
+      {/* 하단의 컴포넌트가 최상단으로 보임 */}
+      {/* absolute는 기존요소와 겹쳐보이도록 */}
+      <View
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          backgroundColor: 'rgba(0,0,0,0.7)',
+          // modal박스를 absolute주지 않고 상하중앙정렬
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            // position: 'absolute',
+            // top: 50,
+            // bottom: 50,
+            // left: 50,
+            // right: 50,
+            borderRadius: 20,
+            padding: 20,
+            backgroundColor: 'wheat',
+            // absolute는 최대한 안쓰는게 좋기 때문에
+            height: 300,
+            width: WINDOW_WIDTH - 100,
+            alignSelf: 'center',
+            // 그림자효과
+            shadowColor: 'black',
+            shadowOpacity: 0.5,
+            shadowRadius: 5,
+            shadowOffset: {width: 5, height: 5},
+            elevation: 15,
+          }}>
+          <View style={{flex: 1, backgroundColor: 'white'}}>
+            <Text>본문</Text>
+            <View
+              style={{
+                position: 'relative',
+                top: 20,
+                backgroundColor: 'tomato',
+              }}>
+              <Text style={{color: 'white', textAlign: 'center'}}>
+                Relative
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              height: 50,
+              // 형제요소에 flex:1을 줬기 때문에 absolute는 필요없음
+              // position: 'absolute',
+              // bottom: 20,
+              // left: 20,
+            }}>
+            <Pressable
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'tomato',
+              }}>
+              <Text style={{color: 'white'}}>YES</Text>
+            </Pressable>
+            <Pressable
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'royalblue',
+              }}>
+              <Text style={{color: 'white'}}> NO</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
     </NavigationContainer>
   );
 }
